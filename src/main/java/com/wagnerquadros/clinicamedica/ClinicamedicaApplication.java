6295094cf6841c5a2c0ac2c68af1e8ac0085d191
wagnerquadros <woquadros@gmail.com>
2026-05-02 21:46:36 -0300

    feat: commit inicial

---
+++ src/main/java/com/wagnerquadros/clinicamedica/ClinicamedicaApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerquadros.clinicamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicamedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicamedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerquadros/clinicamedica/ClinicamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerquadros.clinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
