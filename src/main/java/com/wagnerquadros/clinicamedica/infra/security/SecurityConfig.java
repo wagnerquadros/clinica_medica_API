ab1977d0d4b982fa0f68d3418d05cfd90405a5d0
wagnerquadros <woquadros@gmail.com>
2026-05-12 19:08:08 -0300

    feat: autentucçação via token JWT e configuração Spring Security

--- src/main/java/com/wagnerquadros/clinicamedica/infra/security/SecurityConfig.java
+++ src/main/java/com/wagnerquadros/clinicamedica/infra/security/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.wagnerquadros.clinicamedica.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,16 +13,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/com/wagnerquadros/clinicamedica/infra/security/TokenService.java
+++ src/main/java/com/wagnerquadros/clinicamedica/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.wagnerquadros.clinicamedica.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.wagnerquadros.clinicamedica.entity.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+
+import static com.auth0.jwt.JWT.require;
 
 @Service
 public class TokenService {
@@ -30,6 +33,20 @@ public class TokenService {
             throw new RuntimeException("erro ao gerar Token", exception);
         }
 
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Clínica Médica")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado");
+        }
     }
 
     private Instant dataExpiracao() {
